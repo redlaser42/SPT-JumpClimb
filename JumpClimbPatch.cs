d55f36ddbe42ed464e4d4e9696ed7724f53bc2f0
redlaser42 <120990408+redlaser42@users.noreply.github.com>
2024-08-04 16:46:44 -0400

    Update JumpClimbPatch.cs
    made the red squiggly go away

--- JumpClimbPatch.cs
+++ JumpClimbPatch.cs
@@ -10,22 +10,27 @@ using EFT;
 using EFT.Vaulting;
 using System.Runtime.CompilerServices;
 using SPT.Reflection;
+using BepInEx.Logging;
 
 
 namespace JumpClimb.Patches
 {
+
     //Removes the ginterface207_0.IsGrounded check from method_4. Line 159
     internal class IsGroundedPatch : ModulePatch
     {
+        public static ManualLogSource LogSource;
+
         protected override MethodBase GetTargetMethod()
         {
             return AccessTools.Method(typeof(GClass2171), nameof(GClass2171.method_4));
         }
 
         [PatchPrefix]
-        static bool Prefix(GClass2171 __instance, ref IVaultingSettings __IVaultSettings, GClass2208 ___gclass2208_0, GInterface207 ___GInterface207_0, ref bool __result)
+        static bool Prefix(GClass2171 __instance, IVaultingSettings ___ivaultingSettings_0, GClass2208 ___gclass2208_0, GInterface207 ___ginterface207_0, ref bool __result)
         {
-            __result = __IVaultSettings.IsActive && !__instance.IsVaulting() && ___GInterface207_0.MovementDirection.y >= 0f && !___gclass2208_0.ObstacleCalculatorModel.IsTerrain;
+            __result = ___ivaultingSettings_0.IsActive && !__instance.IsVaulting() && ___ginterface207_0.MovementDirection.y >= 0f && ___ginterface207_0.IsGrounded && !___gclass2208_0.ObstacleCalculatorModel.IsTerrain;
+            LogSource.LogInfo(__result);
             return false;
         }
     }
@@ -33,33 +38,36 @@ namespace JumpClimb.Patches
     //Removes the ginterface207_0.PlayerAnimatorIsJumpSetted()) check from GetVaultingStrategy. Line 228
     internal class GetVaultingStrategyPatch : ModulePatch
     {
+        public static ManualLogSource LogSource;
+
         protected override MethodBase GetTargetMethod()
         {
             return AccessTools.Method(typeof(GClass2171), nameof(GClass2171.GetVaultingStrategy));
         }
 
         [PatchPrefix]
-        static bool Prefix(GClass2171 __instance, ref EVaultingStrategy __result, GClass2208 ___gclass2208_0, GInterface207 ___GInterface207_0, Func<bool> __func_1, Func<bool> __func_2)
+        static bool Prefix(GClass2171 __instance, ref EVaultingStrategy __result, GClass2208 ___gclass2208_0, GInterface207 ___ginterface207_0, Func<bool> ___func_1, Func<bool> ___func_2)
         {
             if (!__instance.method_4())
             {
                 __result = EVaultingStrategy.None;
             }
             bool flag = ___gclass2208_0.StairsCalculatorModel.IsStairsCondition();
-            bool flag2 = ___GInterface207_0.IsSprintEnabled && __instance.BehindObstacleRatio > -0.5f;
-            if (___GInterface207_0.IsAnimatorInTransitionState(0))
+            bool flag2 = ___ginterface207_0.IsSprintEnabled && __instance.BehindObstacleRatio > -0.5f;
+            if (___ginterface207_0.IsAnimatorInTransitionState(0) || ___ginterface207_0.PlayerAnimatorIsJumpSetted())
             {
                 __result = EVaultingStrategy.None;
             }
-            if (__func_1() && ___gclass2208_0.VaultingStatesModel.VaultState.CanMove() && !flag2 && !flag)
+            if (___func_1() && ___gclass2208_0.VaultingStatesModel.VaultState.CanMove() && !flag2 && !flag)
             {
                 __result = EVaultingStrategy.Vault;
             }
-            if (__func_2() && ___gclass2208_0.VaultingStatesModel.ClimbState.CanMove() && !___GInterface207_0.IsSprintEnabled && !flag)
+            if (___func_2() && ___gclass2208_0.VaultingStatesModel.ClimbState.CanMove() && !___ginterface207_0.IsSprintEnabled && !flag)
             {
                 __result = EVaultingStrategy.Climb;
             }
             __result = EVaultingStrategy.None;
+            LogSource.LogInfo(__result);
 
             return false;
         }
